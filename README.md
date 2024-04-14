c7432784b0a4586b4fa89073570685a5f37c9f86
Sebastian Gössl <goessl@student.tugraz.at>
2024-04-14 19:55:29 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,122 @@
+# sqrtfractions
+
+Python module to handle linear combinations of square roots (field extension of rational numbers over integer square roots) exactly.
+A single class, `SqrtFraction`, is provided, to create object of the form
+
+$$
+    \frac{\sum_in_i\sqrt{r_i}}{d} \qquad n_i\in\mathbb{Z}, \ r_i\in\mathbb{N}^+, \ d\in \mathbb{Z}\setminus\{0\}
+$$
+
+
+where the values are represented by Pythons built-in arbitrary size integers, meaning there is no theoretical limit in magnitude nor precision. It's meant to be a next step to Pythons `fraction/Fraction`.
+
+## Usage
+
+A `SqrtFraction` object can be initialized in two ways:
+- with the constructor `SqrtFraction(n={}, d=1)`. The numerator `n` can be given as an integer or as a dictionary of keys:values that correspond to the radicand:factor terms (`SqrtFraction{2:3, 5:-7})` for $\frac{3\sqrt{2}-7\sqrt{5}}{1}$).
+- by the random factory `SqrtFraction.random(N=10, precision=20)`.
+```python
+>>> from _sqrtfractions import SqrtFraction
+>>> SqrtFraction(5)
+SqrtFraction{(+5√1)/1}
+>>> SqrtFraction(5, 2)
+SqrtFraction{(+5√1)/2}
+>>> SqrtFraction({2:3, 5:7}, 2)
+SqrtFraction{(+3√2+7√5)/2}
+```
+
+The objects can be printed
+- in Unicode by `__repr__`
+- in Latex by `_repr_latex_`.
+
+The objects can be casted to
+- `float`s
+- `int`s
+```python
+>>> float(SqrtFraction(5, 2))
+2.5
+>>> int(SqrtFraction(5))
+5
+>>> int(SqrtFraction(5, 2))
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+  File "sqrtfractions.py", line 93, in __int__
+    raise ValueError('doesn\'t represent an integer')
+ValueError: doesn't represent an integer
+```
+
+The objects can be simplified with `reduce`:
+```python
+>>> s = SqrtFraction({4:1, 5:0}, -2)
+>>> s
+SqrtFraction{(+1√4+0√5)/-2}
+>>> s.reduce()
+SqrtFraction{(-1√1)/1}
+```
+
+Basic arithmetic operations are implemented:
+- unary negation `-` and inversion (reciprocal value) `~`
+- addition `+` and subtraction `-`, multiplication * and division `/` with other `SqrtFraction`s and with `int`s.
+```python
+>>> s, t = SqrtFraction({2:3}, 4), SqrtFraction({5:6}, 7)
+>>> s
+SqrtFraction{(+3√2)/4}
+>>> t
+SqrtFraction{(+6√5)/7}
+>>> -s
+SqrtFraction{(+3√2)/-4}
+>>> ~s
+SqrtFraction{(-12√2)/-18}
+>>> s+t
+SqrtFraction{(+21√2+24√5)/28}
+>>> s+5
+SqrtFraction{(+3√2+20√1)/4}
+>>> s-t
+SqrtFraction{(-21√2+24√5)/-28}
+>>> s-5
+SqrtFraction{(+3√2-20√1)/4}
+>>> s*t
+SqrtFraction{(+18√10)/28}
+>>> s*5
+SqrtFraction{(+15√2)/4}
+>>> s/t
+SqrtFraction{(-126√10)/-720}
+>>> s/5
+SqrtFraction{(+3√2)/20}
+```
+
+For more precise descriptions of the methods please refer to the docstrings.
+
+## Design choices
+
+- Not reducing a new `SqrtFraction` directly after initialization: Reducing a `SqrtFraction` is computationally way more demanding than other operations like addition. When many operations have to be done, it is faster to do all of them directly one after another, even if the intermediate results may grow quickly, and then reducing the final result.
+
+## TODO
+
+- Arithmetic with floats (cast self to float and then use float arithmetic, like `fractions/Fraction`)
+- Complexity analysis. Especially of `reduce`.
+- `abs` & `sign` methods. Might be difficult:
+  [Square-root sum problem - Wikipedia](https://en.wikipedia.org/wiki/Square-root_sum_problem)
+  [Sum of radicals - Wikipedia](https://en.wikipedia.org/wiki/Sum_of_radicals#:~:text=The%20sum%20of%20radicals%20is,finite%20linear%20combination%20of%20radicals%3A&text=are%20real%20numbers.)
+
+## License (MIT)
+
+Copyright (c) 2024 Sebastian Gössl
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
---
+++ sqrtfractions.py
@@ -0,0 +1,290 @@
+from math import sqrt, gcd, sumprod
+from collections import defaultdict
+from random import randint
+from itertools import product, islice
+from functools import cache
+
+
+@cache
+def _reduce_sqrt(r):
+    """Return `s, t` such that `sqrt(r)=s*sqrt(t)`.
+    
+    The result has the lowest radicand possible.
+    `r` must be a non-negative integer.
+    """
+    s, i = 1, 2 #factor in front, test factor
+    while i**2 <= r:
+        while not r % i**2:
+            r //= i**2
+            s *= i
+        i += 1
+    return s, r
+
+
+
+class SqrtFraction:
+    r"""Class to handle numbers in the form $\sum_in_i\sqrt{r_i}/d$.
+    
+    The radicands are integers greater or equal 1,
+    the factors infront of them are integers,
+    the denominator is a non-zero integer.
+    This class is immutable.
+    """
+    
+    def __init__(self, n={}, d=1):
+        """Construct a new SqrtFraction.
+        
+        The numerator `n` is expected to be a dictionary with r_i:n_i as
+        keys:values where the radicands must be integers >=1
+        and the factors must be integers.
+        `n` can also be an integer for a usual fraction.
+        The denominator `d` must be a non-zero integer.
+        No arguments defaults to 0.
+        """
+        if isinstance(n, int):
+            self.n, self.d = {1:n}, d
+        elif isinstance(n, dict):
+            assert all(isinstance(k, int) and k>0 for k in n.keys())
+            assert all(isinstance(v, int) for v in n.values())
+            assert isinstance(d, int) and d
+            self.n, self.d = n, d
+        else:
+            raise TypeError
+    
+    @staticmethod
+    def random(N=10, precision=20):
+        r"""Return a random SqrtFraction.
+        
+        The factors from $\sqrt{1}$ up to $\\sqrt{N}$ (incl.)
+        and the denominator will be initialised with random integers `ni`
+        such that `-precision//2 <= ni <= +precision//2`.
+        """
+        n = {n:randint(-precision//2, +precision//2) for n in range(1, N+1)}
+        #https://stackoverflow.com/a/69425586/7367030
+        d = randint(-precision//2, +precision//2-1) or +precision//2
+        return SqrtFraction(n, d)
+    
+    
+    def __float__(self):
+        """Calculate the float approximation.
+        
+        The float value might overflow. Then a copy of this object
+        is first reduced and then calculating the float value is tried again.
+        If this also overflows an OverflowError is raised.
+        """
+        try:
+            #cast to float in case of and empty dict (then sum would be int)
+            return float(sumprod(self.n.values(), map(sqrt, self.n.keys()))) \
+                    / self.d
+        except OverflowError:
+            #return float(self.reduce()) may recourse indefinitely
+            self = self.reduce()
+            return float(sumprod(self.n.values(), map(sqrt, self.n.keys()))) \
+                    / self.d
+    
+    def __int__(self):
+        """Return the integer value.
+        
+        If this object doesn't represent an integer value
+        a ValueError is raised.
+        """
+        self = self.reduce()
+        if set(self.n.keys())<={1} and self.d==1:
+            return self.n.get(1, 0)
+        else:
+            raise ValueError('doesn\'t represent an integer')
+    
+    
+    def __repr__(self):
+        """Return a Unicode representation."""
+        n = [f'{v:+d}{chr(0x221A)}{k}' for k, v in self.n.items()]
+        return 'SqrtFraction{(' + ''.join(n) + f')/{self.d}}}'
+    
+    def _repr_latex_(self):
+        """Return a Latex representation."""
+        n = [f'{v:+d}\\sqrt{{{k}}}' for k, v in self.n.items()]
+        return '$\\frac{' + (''.join(n) if n else '0') + f'}}{{{self.d}}}$'
+    
+    
+    def reduce(self):
+        """Return a mathematically simplified but equivalent `SqrtFraction`.
+        
+        The radicands in the square roots are reduced
+        by pulling square factors out infront.
+        Terms with a zero coefficient are removed.
+        The fraction is shortened.
+        A negative sign of the denominator is brought to the numerator.
+        The square roots are sorted in ascending order.
+        """
+        #simplify numerator
+        n = defaultdict(int)
+        for k, v in self.n.items():
+            f, k = _reduce_sqrt(k)
+            n[k] += f * v
+        n = {k:v for k, v in n.items() if v}
+        #short fraction
+        d = self.d
+        while (cd := gcd(*n.values(), d)) > 1:
+            n = {k:v//cd for k, v in n.items()}
+            d //= cd
+        #aesthetics
+        if d < 0:
+            n, d = {k:-v for k, v in n.items()}, -d
+        return SqrtFraction(dict(sorted(n.items())), d)
+    
+    
+    #Implement the main arithmetic operations (+, *) completely
+    #with type checks and own integer calculations (don't just wrap the integer
+    #into a SqrtFraction, this way it might be a little bit faster).
+    #Just type checks for inverse arithmetic (-, /) to provide better
+    #error strings.
+    #Reuse commutative versions (radd, rmul) completely.
+    def __add__(self, other):
+        """Return the sum with an other `SqrtFraction` or `int`."""
+        if isinstance(other, SqrtFraction):
+            n = defaultdict(int)
+            for k, v in self.n.items():
+                n[k] += v * other.d
+            for k, v in other.n.items():
+                n[k] += v * self.d
+            return SqrtFraction(n, self.d*other.d)
+        elif isinstance(other, int):
+            n = defaultdict(int, self.n)
+            n[1] += other * self.d
+            return SqrtFraction(n, self.d)
+        else:
+            raise TypeError('can only add SqrtFraction or int'
+                    + f' (not "{type(other).__name__}") to SqrtFraction')
+    __radd__ = __add__
+    
+    def __neg__(self):
+        """Return the negation."""
+        return SqrtFraction(self.n, -self.d)
+    
+    def __sub__(self, other):
+        """Return the difference of an other `SqrtFraction` or `int`."""
+        if isinstance(other, SqrtFraction) or isinstance(other, int):
+            return self + (-other)
+        else:
+            raise TypeError('can only subtract SqrtFraction or int'
+                    + f' (not "{type(other).__name__}") from SqrtFraction')
+    
+    def __rsub__(self, other):
+        """Return the difference from an other `SqrtFraction` or `int`."""
+        return (-self) + other
+    
+    def __mul__(self, other):
+        """Return the product with an other `SqrtFraction` or `int`."""
+        if isinstance(other, SqrtFraction):
+            n = defaultdict(int)
+            for ki, vi in self.n.items():
+                for kj, vj in other.n.items():
+                    n[ki*kj] += vi * vj
+            return SqrtFraction(n, self.d*other.d)
+        elif isinstance(other, int):
+            return SqrtFraction({k:v*other for k, v in self.n.items()}, self.d)
+        else:
+            raise TypeError('can only multiply SqrtFraction or int'
+                    + f' (not "{type(other).__name__}") with SqrtFraction')
+    __rmul__ = __mul__
+    
+    def __invert__(self):
+        """Return the reciprocal."""
+        #https://www.youtube.com/watch?v=SjP6Mer0aL8
+        #https://en.wikipedia.org/wiki/Rationalisation_(mathematics)
+        t, r = {k:v for k, v in self.n.items() if v}, SqrtFraction(1)
+        for p in islice(product((+1, -1), repeat=len(t)), 1, None):
+            r *= SqrtFraction({k:s*v for (k, v), s in zip(t.items(), p)})
+        return self.d * r / int(r * SqrtFraction(t))
+    
+    def __truediv__(self, other):
+        """Return the quotient with an other `SqrtFraction` or `int`."""
+        if isinstance(other, SqrtFraction):
+            return self * ~other
+        elif isinstance(other, int):
+            return SqrtFraction(self.n, self.d*other)
+        else:
+            raise TypeError('can only divide SqrtFraction by'
+                    + f' SqrtFraction or int (not "{type(other).__name__}")')
+    
+    def __rtruediv__(self, other):
+        """Return the quotient from an other `SqrtFraction` or `int`."""
+        return ~self * other
+
+
+
+if __name__ == '__main__':
+    assert _reduce_sqrt(0) == (1, 0)
+    assert _reduce_sqrt(1) == (1, 1)
+    assert _reduce_sqrt(2) == (1, 2)
+    assert _reduce_sqrt(3) == (1, 3)
+    assert _reduce_sqrt(4) == (2, 1)
+    assert _reduce_sqrt(5) == (1, 5)
+    assert _reduce_sqrt(6) == (1, 6)
+    assert _reduce_sqrt(7) == (1, 7)
+    assert _reduce_sqrt(8) == (2, 2)
+    assert _reduce_sqrt(9) == (3, 1)
+    assert _reduce_sqrt(10) == (1, 10)
+    assert _reduce_sqrt(11) == (1, 11)
+    assert _reduce_sqrt(12) == (2, 3)
+    assert _reduce_sqrt(13) == (1, 13)
+    assert _reduce_sqrt(14) == (1, 14)
+    assert _reduce_sqrt(15) == (1, 15)
+    assert _reduce_sqrt(16) == (4, 1)
+    assert _reduce_sqrt(17) == (1, 17)
+    assert _reduce_sqrt(18) == (3, 2)
+    assert _reduce_sqrt(19) == (1, 19)
+    assert _reduce_sqrt(20) == (2, 5)
+    
+    
+    
+    from math import isclose as iscl
+    
+    def isclose(a, *b, rel_tol=1e-09, abs_tol=0.0):
+        return all(iscl(a, bi, rel_tol=rel_tol, abs_tol=abs_tol) for bi in b)
+    
+    
+    #reduce
+    for _ in range(1000):
+        a = SqrtFraction.random()
+        assert isclose(float(a), float(a.reduce()))
+    
+    #add
+    for _ in range(1000):
+        a, b = SqrtFraction.random(), SqrtFraction.random()
+        assert isclose(float(a)+float(b), float(a+b))
+        
+        a, b = SqrtFraction.random(), randint(-20, +20)
+        assert isclose(float(a)+b, float(a+b), float(b+a))
+    
+    #sub
+    for _ in range(1000):
+        a, b = SqrtFraction.random(), SqrtFraction.random()
+        assert isclose(float(a)-float(b), float(a-b))
+        
+        a, b = SqrtFraction.random(), randint(-20, +20)
+        assert isclose(float(a)-b, float(a-b), -float(b-a))
+    
+    #mul
+    for _ in range(1000):
+        a, b = SqrtFraction.random(), SqrtFraction.random()
+        assert isclose(float(a)*float(b), float(a*b))
+        
+        a, b = SqrtFraction.random(4), randint(-20, +20)
+        assert isclose(float(a)*b, float(a*b), float(b*a))
+    
+    #invert
+    for _ in range(10):
+        a = SqrtFraction.random(5)
+        assert isclose(1/float(a), float(~a), rel_tol=1e-5)
+    
+    #div
+    for _ in range(10):
+        a, b = SqrtFraction.random(5), SqrtFraction.random(5)
+        assert isclose(float(a)/float(b), float(a/b), rel_tol=1e-5)
+        
+        a, b = SqrtFraction.random(5), randint(-20, +20-1) or +20
+        assert isclose(float(a)/float(b), float(a/b))
+        
+        a, b = SqrtFraction.random(5), randint(-20, +20)
+        assert isclose(float(b)/float(a), float(b/a), rel_tol=1e-5)
